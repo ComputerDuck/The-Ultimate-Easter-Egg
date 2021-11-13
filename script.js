efea924e1cb24068c91461abab0b080708f82956
Computer Duck <84136629+ComputerDuck@users.noreply.github.com>
2021-11-12 22:58:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,15 @@
     The following code is free to use for everyone
 */
 
-function appear() {document.getElementById("bocn").style.animation = "bocn-appear 0.7s forwards";}
-function collapse() {document.getElementById("bocn").style.animation = "bocn-collapse 0.7s forwards";}
 let a = false;
+function appear() {
+        document.getElementById("bocn").style.animation = "bocn-appear 0.7s forwards";
+        a=true;
+}
+function collapse() {
+        document.getElementById("bocn").style.animation = "bocn-collapse 0.7s forwards";
+        a=false;
+}
 document.addEventListener('keydown', function (event) {
   if (event.key === "e") {
     if (a==false) {
